386967b70403cadb507d9a96be1d4f6b9d36d7f3
X.F.Zhou <mrchip.zxf@outlook.com>
2021-11-03 20:53:35 +0800

    BLD: regenerate the backend project template

--- okr/src/main/java/simple/okr/OkrApplication.java
+++ okr/src/main/java/simple/okr/OkrApplication.java
@@ -4,8 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class App {
-    public static void main(String[] args) {
-        SpringApplication.run(App.class, args);
-    }
+public class OkrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OkrApplication.class, args);
+	}
+
 }
